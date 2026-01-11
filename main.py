0b540b934c751d92274ad547ccbfff2ffeb87a73
Korbin Vanette <116692587+KorbinVanetteDev@users.noreply.github.com>
2026-01-11 02:57:34 -0500

    Initialize Flask app with home route

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "GoodSoil Development from end in progress! Check updated for more information!"
+
+if __name__ == '__main__':
+    app.run()
